160d9bece580c778fce532c70237babf03480be0
rashadmemmedov <talehg2932@gmail.com>
2022-05-15 16:02:42 +0400

    buttonlarin yazilmasi

--- src/components/galareya/index.js
+++ src/components/galareya/index.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react';
+import {sliderData} from './slider.js'
 import './style.css';
-import Module from './style.module.css'
+import Module from './style.module.css';
+import {FaArrowLeft,FaArrowRight} from 'react-icons/fa';
 
 const Galareya = () => {
     const [imginfo,setImgInfo]=useState([
@@ -46,17 +48,34 @@ const Galareya = () => {
         },
        
     ]);
+    const[current,setCurrent]=useState(0)
     const imgSrc=useRef();
     const showBigImg=(e)=>{
           imgSrc.current.src=e.target.src;     
     }
+   const changeLeft=()=>{
+       imgSrc.current.src=process.env.PUBLIC_URL +`/img/${sliderData[current].image}`;
+       setCurrent(current===0?sliderData.length-1:current-1);
+       console.log(current);
+   }
+   const changeRight=()=>{
+       setCurrent(current===sliderData.length-1?0:current+1)
+       imgSrc.current.src=process.env.PUBLIC_URL +`/img/${sliderData[current].image}`;
+       console.log(current);
+}
   return (
    <div className={`${Module.flexCenter} countainer`}>
        <div className="blog">
            <div className={`${Module.flexCenter} galeriya`}>
+           <div className="right-btn">
+                   <FaArrowRight onClick={changeRight}/>
+                </div>
                <div className='big-show'>
-                  <img  ref={imgSrc} src={process.env.PUBLIC_URL + `/img/dev-img1.jpg`} alt={imginfo.id} />
+                  <img  ref={imgSrc} src={process.env.PUBLIC_URL + `/img/dev-img7.jpg`} alt={imginfo.id} />
                </div>
+               <div className="left-btn">
+                   <FaArrowLeft onClick={changeLeft}/>
+                </div>
                <div className="show-collection">
                     <div className={`${Module.flexCenter} collection`}>
                        {
@@ -65,7 +84,7 @@ const Galareya = () => {
                         <div className="items" key={index} >
                            { <img 
                                 key={index}
-                                src={process.env.PUBLIC_URL + `/img/dev-img${item.id}.jpg`} 
+                                src={process.env.PUBLIC_URL+`/img/${sliderData[index].image}`} 
                                 alt={item.title} 
                                 onClick={showBigImg}
                             /> }
@@ -81,4 +100,4 @@ const Galareya = () => {
   )
 }
 
-export default Galareya
+export default Galareya;
---
+++ src/components/galareya/slider.js
@@ -0,0 +1,13 @@
+export  const sliderData=[
+    {image:'dev-img1.jpg'},
+    {image:'dev-img2.jpg'},
+    {image:'dev-img3.jpg'},
+    {image:'dev-img4.jpg'},
+    {image:'dev-img5.jpg'},
+    {image:'dev-img6.jpg'},
+    {image:'dev-img7.jpg'},
+    {image:'dev-img8.jpg'},
+    {image:'dev-img9.jpg'},
+    {image:'dev-img10.jpg'},
+    
+]
